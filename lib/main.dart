c54f46ce8d92758d4c9082d91dbaac4776b84ed6
User Namegit config --global user.name User <mohamedswylam9@gmail.com>
2022-04-01 03:28:35 +0200

    Stripe

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      builder: EasyLoading.init(),
     );
   }
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import '../service/payment/payment-service.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  onItemPress(BuildContext context, int index) async {
+    switch(index) {
+      case 0:
+        Navigator.pushNamed(context, '/pay-with-new-card');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/existing-cards');
+        break;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    StripeService.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              Icon icon = Icon(Icons.add_circle);
+              Text text = Text('');
+
+              switch(index) {
+                case 0:
+                  icon = Icon(Icons.add_circle, color: theme.primaryColor);
+                  text = Text('Pay via new card');
+                  break;
+                case 1:
+                  icon = Icon(Icons.credit_card, color: theme.primaryColor);
+                  text = Text('Pay via existing card');
+                  break;
+              }
+
+              return InkWell(
+                onTap: () {
+                  onItemPress(context, index);
+                },
+                child: ListTile(
+                  title: text,
+                  leading: icon,
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => Divider(
+              color: theme.primaryColor,
+            ),
+            itemCount: 2
+        ),
+      ),
+    );
+  }
+}
