fa29602597b30bfbcb00daa6029006e6ec9c327b
User Namegit config --global user.name User <mohamedswylam9@gmail.com>
2022-04-02 02:53:04 +0200

    stripe

--- lib/service/payment/payment-service.dart
+++ lib/service/payment/payment-service.dart
@@ -1,145 +1,105 @@
 import 'dart:convert';
-import 'package:flutter/services.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-class StripeTransactionResponse {
-  String message;
-  bool success;
-  StripeTransactionResponse({required this.message, required this.success});
-}
-
-class CreditCard {}
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 class StripeService {
   static String apiBase = 'https://api.stripe.com/v1';
-  static String paymentApiUrl = '${StripeService.apiBase}/payment_intents';
-  static String secret = 'your_api_secret';
+  static Uri paymentApiUrl = Uri.parse('https://api.stripe.com/v1/payment_intents');
+  static String secret =
+      'sk_test_51Kc85hHQzMPK5sWEriyOzwhAjVMpw8UZlygCAZT6y1YF6KJngVblHJj5g4OhOuC6rOcBhL9C4aikkVTUzlWby55M00R5pKEJ02';
   static Map<String, String> headers = {
-    'Authorization': 'Bearer ${StripeService.secret}',
-    'Content-Type': 'application/x-www-form-urlencoded'
+    'Authorization': 'Bearer${StripeService.secret}',
+    'Content-Type': 'application/x-www-form-urlencoded',
   };
+  Map<String, dynamic>? paymentIntentData;
+
   static init() {
-    Stripe.publishableKey = 'pk_test_51Kc85hHQzMPK5sWEUUNeBXQNl6pyrqSDrJpaZ2ndO1jGSp9INyPEIvGCdOPLrZx8WroJCiz7nFVFfUd5GjT2nUaj00TCDLmfNk';
-
-    // StripePayment.setOptions(
-    //     StripeOptions(
-    //         publishableKey: "your_api_publishable_key",
-    //         merchantId: "Test",
-    //         androidPayMode: 'test'
-    //     )
-    // );
+    Stripe.publishableKey =
+        'pk_test_51Kc85hHQzMPK5sWEUUNeBXQNl6pyrqSDrJpaZ2ndO1jGSp9INyPEIvGCdOPLrZx8WroJCiz7nFVFfUd5GjT2nUaj00TCDLmfNk';
   }
 
-  static Future<StripeTransactionResponse> payViaExistingCard({required String amount, required String currency, required CreditCard card}) async{
+  Future<void> PayWithNewCardPage(context) async {
     try {
-      // var paymentMethod = await StripePayment.createPaymentMethod(
-      //     PaymentMethodRequest(card: card)
-      // );
-      // var paymentIntent = await StripeService.createPaymentIntent(
-      //     amount,
-      //     currency
-      // );
-      // var response = await StripePayment.confirmPaymentIntent(
-      //     PaymentIntent(
-      //         clientSecret: paymentIntent['client_secret'],
-      //         paymentMethodId: paymentMethod.id
-      //     )
-      // );
-      // if (response.status == 'succeeded') {
-      //   return new StripeTransactionResponse(
-      //       message: 'Transaction successful',
-      //       success: true
-      //   );
-      // } else {
-      //   return new StripeTransactionResponse(
-      //       message: 'Transaction failed',
-      //       success: false
-      //   );
-      // }
-      return new StripeTransactionResponse(
-          message: 'Transaction successful',
-          success: true
-      );
-    } on PlatformException catch(err) {
-      return StripeService.getPlatformExceptionErrorResult(err);
-    } catch (err) {
-      return new StripeTransactionResponse(
-          message: 'Transaction failed: ${err.toString()}',
-          success: false
-      );
+      paymentIntentData =
+          await createPaymentIntent('20', 'USD'); //json.decode(response.body);
+      // print('Response body==>${response.body.toString()}');
+      await Stripe.instance
+          .initPaymentSheet(
+              paymentSheetParameters: SetupPaymentSheetParameters(
+                  paymentIntentClientSecret:
+                      paymentIntentData!['client_secret'],
+                  applePay: true,
+                  googlePay: true,
+                  testEnv: true,
+                  style: ThemeMode.dark,
+                  merchantCountryCode: 'US',
+                  merchantDisplayName: 'ANNIE'))
+          .then((value) {});
+
+      ///now finally display payment sheeet
+      displayPaymentSheet(context);
+    } catch (e, s) {
+      print('exception:$e$s');
     }
   }
 
-  static Future<StripeTransactionResponse> payWithNewCard({required String amount, required String currency}) async {
+  displayPaymentSheet(context) async {
     try {
-      // var paymentMethod = await StripePayment.paymentRequestWithCardForm(
-      //     CardFormPaymentRequest()
-      // );
-      // var paymentIntent = await StripeService.createPaymentIntent(
-      //     amount,
-      //     currency
-      // );
-      // var response = await StripePayment.confirmPaymentIntent(
-      //     PaymentIntent(
-      //         clientSecret: paymentIntent['client_secret'],
-      //         paymentMethodId: paymentMethod.id
-      //     )
-      // );
-      // if (response.status == 'succeeded') {
-      //   return new StripeTransactionResponse(
-      //       message: 'Transaction successful',
-      //       success: true
-      //   );
-      // } else {
-      //   return new StripeTransactionResponse(
-      //       message: 'Transaction failed',
-      //       success: false
-      //   );
-      // }
-      return new StripeTransactionResponse(
-          message: 'Transaction successful',
-          success: true
-      );
-    } on PlatformException catch(err) {
-      return StripeService.getPlatformExceptionErrorResult(err);
-    } catch (err) {
-      return new StripeTransactionResponse(
-          message: 'Transaction failed: ${err.toString()}',
-          success: false
-      );
+      await Stripe.instance
+          .presentPaymentSheet(
+              parameters: PresentPaymentSheetParameters(
+        clientSecret: paymentIntentData!['client_secret'],
+        confirmPayment: true,
+      ))
+          .then((newValue) {
+        print('payment intent' + paymentIntentData!['id'].toString());
+        print(
+            'payment intent' + paymentIntentData!['client_secret'].toString());
+        print('payment intent' + paymentIntentData!['amount'].toString());
+        print('payment intent' + paymentIntentData.toString());
+        //orderPlaceApi(paymentIntentData!['id'].toString());
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("paid successfully")));
+
+        paymentIntentData = null;
+      }).onError((error, stackTrace) {
+        print('Exception/DISPLAYPAYMENTSHEET==> $error $stackTrace');
+      });
+    } on StripeException catch (e) {
+      print('Exception/DISPLAYPAYMENTSHEET==> $e');
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                content: Text("Cancelled "),
+              ));
+    } catch (e) {
+      print('$e');
     }
   }
 
-  static getPlatformExceptionErrorResult(err) {
-    String message = 'Something went wrong';
-    if (err.code == 'cancelled') {
-      message = 'Transaction cancelled';
-    }
-
-    return new StripeTransactionResponse(
-        message: message,
-        success: false
-    );
-  }
-
-  static Future<Map<String, dynamic>?> createPaymentIntent(String amount, String currency) async {
+//  Future<Map<String, dynamic>>
+  createPaymentIntent(String amount, String currency) async {
     try {
       Map<String, dynamic> body = {
-        'amount': amount,
+        'amount': calculateAmount('20'),
         'currency': currency,
         'payment_method_types[]': 'card'
       };
-      // var response = await http.post(
-      //     StripeService.paymentApiUrl,
-      //     body: body,
-      //     headers: StripeService.headers
-      // );
-      // return jsonDecode(response.body);
-      return new Map();
+      print(body);
+      var response = await http.post(
+          paymentApiUrl,
+          body: body,
+          headers: headers);
+      print('Create Intent reponse ===> ${response.body.toString()}');
+      return jsonDecode(response.body);
     } catch (err) {
       print('err charging user: ${err.toString()}');
     }
-    return null;
+  }
+
+  calculateAmount(String amount) {
+    final a = (int.parse(amount)) * 100;
+    return a.toString();
   }
 }
